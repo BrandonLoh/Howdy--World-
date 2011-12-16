b5902204d3397b6fc2d3dcf195bdd78e515d4b8b
Brandon Loh <lubaixun@gmail.com>
2011-12-16 23:22:07 +0800

    Try Vim Plugin Perl-Support

---
+++ hello.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl 
+#===============================================================================
+#
+#         FILE: hello.pl
+#
+#        USAGE: ./hello.pl  
+#
+#  DESCRIPTION: first perl in vim
+#
+#      OPTIONS: 
+#      REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: YOUR NAME (), 
+#      COMPANY: 
+#      VERSION: 1.0
+#      CREATED: 2011年12月16日 23时15分50秒
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+
+print "Howdy, World!\n";
+#
